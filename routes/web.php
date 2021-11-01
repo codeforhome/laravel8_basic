5ab1113ea8dd7a68765ad7c4ed87f54061090820
codeforhome <83600086+codeforhome@users.noreply.github.com>
2021-11-01 13:02:57 +0800

    49. Admin Panel Setup Part 3

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::post('/multi/add', [BrandController::class, 'StoreImg']) -> name('store.i
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    $users = User::all();
+    // $users = User::all();
     // $users = DB::table('users') -> get();
-    return view('dashboard', compact('users'));
+    return view('admin.index');
 })->name('dashboard');
